bde5737291e3cebbafc1933a662287e246479802
jooyeon chung <jy1106.chung@samsung.com>
2019-03-11 09:55:39 +0900

    Test Time Series Decompostion

---
+++ function/python/brightics/function/timeseries/test/test_time_series_decomposition.py
@@ -0,0 +1,33 @@
+import unittest
+import numpy as np
+import numpy.testing as npt
+from brightics.common.datasets import load_iris
+from brightics.function.timeseries import timeseries_decomposition
+
+
+class TestTimeSeriesDecomposition(unittest.TestCase):
+
+    def setUp(self):
+        print("*** Time Series Decomposition UnitTest Start ***")
+        self.iris = load_iris()
+
+    def tearDown(self):
+        print("*** Time Series Decomposition UnitTest End ***")
+
+    def test_time_series_decomposition(self):
+        input_dataframe = self.iris
+        
+        res = timeseries_decomposition(table=input_dataframe, input_col='sepal_length', frequency=4)['out_table']
+        
+        print(res)
+        
+        table = res.values.tolist()
+        npt.assert_array_equal(table[0], [5.1, 3.5, 1.4, 0.2, 'setosa', np.NaN, 0.013989771021021011, np.NaN])
+        npt.assert_array_equal(table[1], [4.9, 3.0, 1.4, 0.2, 'setosa', np.NaN, -0.0537185623123123, np.NaN])
+        npt.assert_array_equal(table[2], [4.7, 3.2, 1.3, 0.2, 'setosa', 4.8125, -0.017128847597597585, -0.09537115240240224])
+        npt.assert_array_equal(table[3], [4.6, 3.1, 1.5, 0.2, 'setosa', 4.8625, 0.056857638888888874, -0.3193576388888891])
+        npt.assert_array_equal(table[4], [5.0, 3.6, 1.4, 0.2, 'setosa', 4.9125000000000005, 0.013989771021021011, 0.07351022897897845])
+
+
+if __name__ == '__main__':
+    unittest.main()
