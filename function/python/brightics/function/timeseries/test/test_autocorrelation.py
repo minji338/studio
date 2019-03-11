6fc9659dfc0ce270bbc712e85cf8dbe0d76e462a
jooyeon chung <jy1106.chung@samsung.com>
2019-03-12 08:29:58 +0900

    AutoCorrelation: add unittest

--- function/python/brightics/function/timeseries/test/test_autocorrelation.py
+++ function/python/brightics/function/timeseries/test/test_autocorrelation.py
@@ -12,7 +12,7 @@ class TestAutoCorrelation(unittest.TestCase):
     def tearDown(self):
         print("*** AutoCorrelation UnitTest End ***")
 
-    def test_autocorrelation(self):
+    def test_autocorrelation1(self):
         input_dataframe = self.iris
         
         res = autocorrelation(table=input_dataframe, input_col='sepal_length')['model']
@@ -30,7 +30,25 @@ class TestAutoCorrelation(unittest.TestCase):
         self.assertListEqual(table1[19], [19, 0.3326828817365676, '(-0.15031337059372796, 0.8156791340668632)'])
         self.assertListEqual(table2[2], [2, 0.446878925378876, '(0.2868485361670323, 0.6069093145907197)'])
         self.assertListEqual(table2[18], [18, 0.057734494528377006, '(-0.10229589468346667, 0.21776488374022068)'])
-
+    
+    def test_autocorrelation2(self):
+        input_dataframe = self.iris
+        
+        res = autocorrelation(table=input_dataframe, input_col='sepal_length', nlags=10, conf_level=0.99)['model']
+        res_autocorrelation=res['autocorrelation_table']
+        res_partial_autocorrelation=res['partial_autocorrelation_table']
+        
+        
+        print("Autocorrelation Table\n", res_autocorrelation.values)
+        
+        print("Partial Autocorrelation Table\n", res_partial_autocorrelation.values)
+        
+        table1=res_autocorrelation.values.tolist()
+        table2=res_partial_autocorrelation.values.tolist()
+        self.assertListEqual(table1[1], [1, 0.5920665785767498, '(0.38175099663663753, 0.8023821605168622)'])
+        self.assertListEqual(table1[9], [9, 0.4261180078628408, '(-0.07603010015333178, 0.9282661158790134)'])
+        self.assertListEqual(table2[2], [2, 0.446878925378876, '(0.23656334343876373, 0.6571945073189883)'])
+        self.assertListEqual(table2[8], [8, 0.10858539870660004, '(-0.10173018323351225, 0.31890098064671235)'])
 
 if __name__ == '__main__':
     unittest.main()
