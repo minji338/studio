24f8c273ef67d73d5f104008a400381f7286f16b
jooyeon chung <jy1106.chung@samsung.com>
2019-03-12 08:39:02 +0900

    Outlier Detection (Tukey/Carling, Local Outlier Factor)
    : add unittest

--- function/python/brightics/function/manipulation/test/test_outlier_detection.py
+++ function/python/brightics/function/manipulation/test/test_outlier_detection.py
@@ -2,7 +2,7 @@ import unittest
 from brightics.common.datasets import load_iris
 from brightics.function.transform import split_data
 from brightics.function.manipulation import outlier_detection_tukey_carling, outlier_detection_tukey_carling_model, outlier_detection_lof, outlier_detection_lof_model
-from sklearn.neighbors import LocalOutlierFactor
+
 
 class TestOutlierDetection(unittest.TestCase):
 
@@ -13,7 +13,7 @@ class TestOutlierDetection(unittest.TestCase):
     def tearDown(self):
         print("*** Outlier Detection UnitTest End ***")
 
-    def test_outlier_detection_tukey_carling(self):
+    def test_outlier_detection_tukey_carling1(self):
         input_dataframe = self.iris
         
         df_splitted = split_data(table=input_dataframe, train_ratio=7.0, test_ratio=3.0, random_state=1)
@@ -33,14 +33,35 @@ class TestOutlierDetection(unittest.TestCase):
         self.assertListEqual(table[29], [6.3, 2.5, 5, 1.9, 'virginica', 'in', 'in', 'in', 'in'])
         self.assertListEqual(table[30], [6.1, 3, 4.6, 1.4, 'versicolor', 'in', 'in', 'in', 'in'])
         
-    def test_outlier_detection_lof(self):
+    def test_outlier_detection_tukey_carling2(self):
         input_dataframe = self.iris
         
         df_splitted = split_data(table=input_dataframe, train_ratio=7.0, test_ratio=3.0, random_state=1)
         train_df = df_splitted['train_table']
         test_df = df_splitted['test_table']
         
-        res_train = outlier_detection_lof(table=train_df,input_cols=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+        res_train = outlier_detection_tukey_carling(table=train_df,
+                                              input_cols=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'], outlier_method='carling', multiplier=2, number_of_removal=1,
+                                    result_type='both', new_column_prefix='in_or_out')
+        res_model = outlier_detection_tukey_carling_model(table=test_df, model=res_train['model'], new_column_prefix='in_or_out')
+        
+        print(res_model['out_table'])
+        
+        table = res_model['out_table'].values.tolist()
+        self.assertListEqual(table[0], [5.8, 4, 1.2, 0.2, 'setosa', 'in', 'in', 'in', 'in'])
+        self.assertListEqual(table[1], [5.1, 2.5, 3, 1.1, 'versicolor', 'in', 'in', 'in', 'in'])
+        self.assertListEqual(table[2], [6.6, 3, 4.4, 1.4 , 'versicolor', 'in', 'in', 'in', 'in'])
+        self.assertListEqual(table[3], [5.4, 3.9, 1.3, 0.4, 'setosa', 'in', 'in', 'in', 'in'])
+        self.assertListEqual(table[4], [7.9, 3.8, 6.4, 2, 'virginica', 'in', 'in', 'in', 'in'])
+        
+    def test_outlier_detection_lof1(self):
+        input_dataframe = self.iris
+        
+        df_splitted = split_data(table=input_dataframe, train_ratio=7.0, test_ratio=3.0, random_state=1)
+        train_df = df_splitted['train_table']
+        test_df = df_splitted['test_table']
+        
+        res_train = outlier_detection_lof(table=train_df, input_cols=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
         res_model = outlier_detection_lof_model(table=test_df, model=res_train['model'])
         
         print(res_model['out_table'])
@@ -51,6 +72,25 @@ class TestOutlierDetection(unittest.TestCase):
         self.assertListEqual(table[2], [6.6, 3, 4.4, 1.4 , 'versicolor', 'in'])
         self.assertListEqual(table[3], [5.4, 3.9, 1.3, 0.4, 'setosa', 'in'])
         self.assertListEqual(table[4], [7.9, 3.8, 6.4, 2, 'virginica', 'out'])
+        
+    def test_outlier_detection_lof2(self):
+        input_dataframe = self.iris
+        
+        df_splitted = split_data(table=input_dataframe, train_ratio=7.0, test_ratio=3.0, random_state=1)
+        train_df = df_splitted['train_table']
+        test_df = df_splitted['test_table']
+        
+        res_train = outlier_detection_lof(table=train_df, input_cols=['sepal_length', 'sepal_width', 'petal_length', 'petal_width'], n_neighbors=10, result_type='both', new_column_name='is_or_out')
+        res_model = outlier_detection_lof_model(table=test_df, model=res_train['model'], new_column_name='in_or_out')
+        
+        print(res_model['out_table'])
+        
+        table = res_model['out_table'].values.tolist()
+        self.assertListEqual(table[0], [5.1, 2.5, 3, 1.1, 'versicolor', 'in'])
+        self.assertListEqual(table[1], [6.6, 3, 4.4, 1.4, 'versicolor', 'in'])
+        self.assertListEqual(table[2], [5.4, 3.9, 1.3, 0.4 , 'setosa', 'in'])
+        self.assertListEqual(table[3], [7.9, 3.8, 6.4, 2, 'virginica', 'in'])
+        self.assertListEqual(table[4], [6.3, 3.3, 4.7, 1.6, 'versicolor', 'in'])
 
 
 if __name__ == '__main__':
