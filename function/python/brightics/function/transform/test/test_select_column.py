4d4a131b1c0a7dbefe6743027ebd9497cd371f3c
jooyeon chung <jy1106.chung@samsung.com>
2019-03-12 09:52:13 +0900

    Select Column: add unittest

---
+++ function/python/brightics/function/transform/test/test_select_column.py
@@ -0,0 +1,43 @@
+import unittest
+import pandas as pd
+import numpy as np
+from brightics.function.transform import select_column
+# from brightics.function.test_data import load_iris 
+
+df_example1 = pd.DataFrame({'num1':[1, 2, 3, 4, 5],
+                            'num2':[10, 20, 30, 40, 50],
+                            'str1':['a', 'b', 'c', 'd', 'e']}) 
+
+df_example2 = pd.DataFrame({'num1':[1, 2, 3, 4, 5, 6],
+                            'num2':[10, 20, 30, np.nan, 50, 60],
+                            'num3':[10.0, np.nan, np.nan, 40.0, 50.0, 60.0],
+                            'str1':['a', 'b', 'c', 'd', 'e', 'f']}) 
+
+
+class TestSelectColumn(unittest.TestCase):
+    
+    def test_select_column1(self):
+        df = df_example1.copy() 
+        out_df = select_column(table=df, input_cols=['num2', 'str1'])['out_table']
+        print(df)
+        print(out_df)
+        
+    def test_select_column2(self):
+        df = df_example1.copy() 
+        out_df = select_column(table=df, input_cols=['num2'], output_cols=['num22'], output_types=['double'])['out_table']
+        print(df)
+        print(df.dtypes)
+        print(out_df)
+        print(out_df.dtypes)
+        
+    def test_select_column3(self):
+        df = df_example1.copy() 
+        out_df = select_column(table=df, input_cols=['num1', 'num2', 'str1'], output_cols=['num11', 'num22'], output_types=['int', 'string', 'double'])['out_table']
+        print(df)
+        print(df.dtypes)
+        print(out_df)
+        print(out_df.dtypes)
+
+        
+if __name__ == '__main__':
+    unittest.main()
